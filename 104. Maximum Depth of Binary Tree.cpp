f6b6ce06059847f0ef1f38d4fb8a1af5a150252b
Shashi Mehta <103140467+shashimehta03@users.noreply.github.com>
2024-04-05 16:37:19 +0530

    Update 104. Maximum Depth of Binary Tree.cpp

--- 104. Maximum Depth of Binary Tree.cpp
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -25,3 +25,15 @@ public:
         return ans;
     }
 };
+//approch 2
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left=maxDepth(root->left);
+        int right=maxDepth(root->right);
+        return max(left,right)+1;
+    }
+};
