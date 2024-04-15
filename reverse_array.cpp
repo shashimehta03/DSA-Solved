50cd918b8fa46c568357e225001482d1600d532d
Shashi Mehta <103140467+shashimehta03@users.noreply.github.com>
2024-04-15 23:55:26 +0530

    Create reverse_array.cpp

---
+++ reverse_array.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    // for(int i=0;i<5;i++){
+    //     for(int j=i+1;j<5;j++){
+    //         cout<<arr[i]<<arr[j]<<" ";
+    //     }
+    // } pair bana ke liya
+    
+    // for(int i=0;i<2;i++){
+    //     int temp=arr[i];
+    //     int last=arr[4-i];
+    //     arr[i]=last;
+    //     arr[4-i]=temp;
+    // } reverse an array
+    
+    // int fir=0;
+    // int last=4;
+    // while(last>fir){
+    //     int temp=arr[fir];
+    //     arr[fir]=arr[last];
+    //     arr[last]=temp;
+    //     fir++;
+    //     last--;
+    // } reverse an array
+    
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
