40c782ab49c920d4440fb3591780113a5b6c0e93
Shashi Mehta <103140467+shashimehta03@users.noreply.github.com>
2024-08-18 16:12:12 +0530

    Update 162. Find Peak Element.cpp

--- 162. Find Peak Element.cpp
+++ 162. Find Peak Element.cpp
@@ -14,3 +14,26 @@ public:
         
     }
 };
+//using binary search
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+        int low=0;
+        int high=n-1;
+
+        while(low<high){
+            int mid=(low+high)/2;            
+            if(nums[mid]<nums[mid+1]) 
+            low=mid+1;
+            else
+            high=mid;
+            
+        }
+        return high;
+    }
+};
